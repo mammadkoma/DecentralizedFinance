50420663f0f6464fe3233599ae162fdb12838dce
MK <komaei@live.com>
2021-02-17 01:31:53 +0330

    Add project files.

---
+++ DecentralizedFinance/Controllers/TokensController.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using DecentralizedFinance.Data;
+using DecentralizedFinance.Data.Models;
+using DecentralizedFinance.ViewModels;
+
+namespace DecentralizedFinance.Controllers
+{
+    public class TokensController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public TokensController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Tokens
+        public async Task<IActionResult> Index()
+        {
+            var Tokens = await _context.Token
+                .Include(i => i.Group)
+                .Select(t => new TokenViewModel
+                {
+                    Id = t.Id,
+                    Name = t.Name,
+                    Price = t.Price,
+                    FullName = t.FullName,
+                    TotalAmount = t.TotalAmount,
+                    Tvl = t.Tvl,
+                    GroupName = t.Group.Name,
+                })
+                .ToListAsync();
+
+            return View(Tokens);
+        }
+
+        // GET: Tokens/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var token = await _context.Token
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (token == null)
+            {
+                return NotFound();
+            }
+
+            return View(token);
+        }
+
+        // GET: Tokens/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Tokens/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Price,FullName,TotalAmount,Tvl,GroupId")] Token token)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(token);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(token);
+        }
+
+        // GET: Tokens/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var token = await _context.Token.FindAsync(id);
+            if (token == null)
+            {
+                return NotFound();
+            }
+            return View(token);
+        }
+
+        // POST: Tokens/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Price,FullName,TotalAmount,Tvl,GroupId")] Token token)
+        {
+            if (id != token.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(token);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TokenExists(token.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(token);
+        }
+
+        // GET: Tokens/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var token = await _context.Token
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (token == null)
+            {
+                return NotFound();
+            }
+
+            return View(token);
+        }
+
+        // POST: Tokens/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var token = await _context.Token.FindAsync(id);
+            _context.Token.Remove(token);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TokenExists(int id)
+        {
+            return _context.Token.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ DecentralizedFinance/ViewModels/TokenViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DecentralizedFinance.ViewModels
+{
+    public class TokenViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double? Price { get; set; }
+        public string FullName { get; set; }
+        public double? TotalAmount { get; set; }
+        public string Tvl { get; set; }
+        public string GroupName { get; set; }
+    }
+}
